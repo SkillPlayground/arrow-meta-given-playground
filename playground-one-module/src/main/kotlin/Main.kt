2c834e10f8cfdde1fa586f90aaa8bcd2cf125686
Javier Segovia Córdoba <javier@segoviacordoba.com>
2021-07-16 23:43:41 +0200

    Try local 2

--- playground-one-module/src/main/kotlin/Main.kt
+++ playground-one-module/src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 package com.javiersc.arrow.meta.given.playground.one.module
 
 fun main() {
-    //    val viewModel: UserViewModel = given()
-    //    println(viewModel.loadUser())
+    val viewModel: UserViewModel = given()
+    println(viewModel.loadUser())
 }
